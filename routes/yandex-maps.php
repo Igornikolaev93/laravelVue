cf6829d141cc891eac441f6bb85a4daa6179bf39
igorn2659 <igorn2659@gmail.com>
2026-02-14 11:57:50 +0000

    yandex-map

--- routes/yandex-maps.php
+++ routes/yandex-maps.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\YandexMapsController;
 
-Route::middleware('auth')->group(function () {
+Route::group([], function () {
     Route::get('/yandex-maps', [YandexMapsController::class, 'index'])->name('yandex-maps.index');
     Route::get('/yandex-maps/settings', [YandexMapsController::class, 'settings'])->name('yandex-maps.settings');
     Route::post('/yandex-maps/settings', [YandexMapsController::class, 'saveSettings'])->name('yandex-maps.save-settings');
