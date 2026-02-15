0f97578bd75c5d3c6cd64fed534e4ee5e62f1ef1
igorn2659 <igorn2659@gmail.com>
2026-02-15 11:02:05 +0000

    sklczd nL:Mxz

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,8 +10,9 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Fonts -->
-    <link rel="dns-prefetch" href="//fonts.gstatic.com">
-    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..700;1,400..700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
 
     <!-- Scripts -->
@@ -21,7 +22,7 @@
         /* Global styles */
         body {
             margin: 0;
-            font-family: 'Mulish', 'Inter', sans-serif;
+            font-family: 'Instrument Sans', sans-serif;
             background-color: #F9F9F9;
         }
 
